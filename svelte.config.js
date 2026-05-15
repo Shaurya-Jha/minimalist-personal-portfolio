1a08c419bc21956b798d1e2da360208d89abb0c7
Shaurya-Jha <shauryajha1602@gmail.com>
2026-05-16 01:57:07 +0530

    minimal v1

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-gh-pages' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
